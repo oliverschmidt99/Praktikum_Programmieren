6f8ae82b2fa33921f84a8fedd09e54d5ff4be6cd
oliverschmidt99 <131264394+oliverschmidt99@users.noreply.github.com>
2023-05-09 16:59:36 +0200

    Update main_a5.c

--- Aufgabe_5/Version_2/main_a5.c
+++ Aufgabe_5/Version_2/main_a5.c
@@ -1,1 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "json.h"
+#include "rational.h"
+#include "json1.c"
 
+
+
+
+int main(int argc, char *argv[])
+{
+    printf(NL NL);
+
+    short error = 0;
+    uint8_t *objekte = NULL;
+    rational *json_rat = creat(0,0,0);
+    char *filename = "json_zahlen.json";
+
+
+    
+    if (argc == 2)
+    {
+        printf(RD"Fehler: "RS"Bitte geben sie eine Datei an, die goeffnet werden soll!"NL NL);
+        return -1;
+    }
+
+    objekte = open_json(filename);
+    printf("%s" NL, objekte);
+
+    error = json_roulette(objekte, json_rat);
+    if (error != 0)
+    {
+        printf(RD"Fehler: "RS"Umformung von Json hat nicht funktioniert!"NL NL);
+        return -1;
+    }
+
+destroy(json_rat);
+printf(NL NL);
+return 0;
+}
